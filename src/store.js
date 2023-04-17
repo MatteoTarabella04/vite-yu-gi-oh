d04eb3824fb104b63ab0fb0d69266e6865a3ca13
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-17 17:21:46 +0200

    import axios + create api call

--- src/store.js
+++ src/store.js
@@ -1,4 +1,22 @@
 import { reactive } from 'vue'
-
+import axios from 'axios';
 export const store = reactive({
-})
+   API_URL: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0',
+   loading: true,
+   cards: [],
+
+
+   callApiCardsPath(url) {
+      axios
+         .get(url)
+         .then(response => {
+            console.log(response);
+            this.cards = response.data.data;
+            console.log(this.cards);
+         })
+         .catch(err => {
+            console.log(err);
+            console.error(err.message);
+         })
+   },
+});
