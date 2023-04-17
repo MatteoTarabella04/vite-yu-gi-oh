f3848b0843886a5c6f1f55afea85e5d108939486
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-17 16:30:42 +0200

    add form select

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,28 @@ export default {
 
 <template>
   <AppHeader />
+  <main>
+    <div class="container px-2 py-4">
+      <select class="form-select" aria-label="default">
+        <option selected>Filtra</option>
+        <option value="1">Alien</option>
+        <option value="2">???</option>
+        <option value="3">???</option>
+      </select>
+    </div>
+    <div class="container bg-light h-100">
+
+    </div>
+  </main>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+  background-color: orange;
+
+  .form-select {
+    width: 200px;
+  }
+}
+</style>
