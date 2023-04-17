b65de5063265d6d2c4d77e44f7480fe797241adf
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-17 17:46:44 +0200

    create card item component

---
+++ src/components/cardItem.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+
+   name: 'cardItem',
+
+   props: {
+      card: Object,
+   },
+
+
+}
+</script>
+<template>
+   <div class="col mb-3">
+      <div class="card rounded-0">
+         <img :src="card.card_images[0].image_url" :alt="card.name" height="300">
+         <div class="info p-2 text-light text-center">
+            <h5 class="m-0">{{ card.name }}</h5>
+            <div class="archetype">
+               <p class="m-0">{{ card.archetype }}</p>
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+
+
+<style lang="scss" scoped></style>
