33984f9de4f9bbbfe190f620c2c40769b8465b7b
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-17 17:46:56 +0200

    cerate card list compponent

---
+++ src/components/cardList.vue
@@ -0,0 +1,34 @@
+<script>
+import { store } from '../store';
+import foundBanner from './foundBanner.vue';
+import cardItem from './cardItem.vue';
+export default {
+   name: 'cardList',
+
+   components: {
+      foundBanner,
+      cardItem,
+   },
+
+   props: {
+      cards: Array,
+   },
+
+   data() {
+      return {
+         store,
+      }
+   }
+}
+</script>
+<template>
+   <div class="container bg-light h-100 p-4">
+      <foundBanner />
+      <div class="row row-cols-5">
+         <cardItem v-for="card in store.cards" :card="card" />
+      </div>
+   </div>
+</template>
+
+
+<style lang="scss" scoped></style>
