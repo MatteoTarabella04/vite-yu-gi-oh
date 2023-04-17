bd2d6f6464d5d6c6ee9f33939a5e0e39441acdd0
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-17 16:12:42 +0200

    add header elements

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+   name: 'appHeader',
+}
+</script>
+<template>
+   <header class="d-flex align-items-center">
+      <div class="logo px-3">
+         <img src="../assets/img/yu-gi-oh-logo.png" alt="" height="100">
+      </div>
+      <div class="title">
+         <h1 class="m-0">Yu-Gi-Oh Api</h1>
+      </div>
+   </header>
+</template>
+
+
+<style lang="scss" scoped></style>
