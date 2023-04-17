84cc5e7a880f1d6e40646d9fcf71a8e99444f8f2
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-17 17:47:11 +0200

    create found banner component

---
+++ src/components/foundBanner.vue
@@ -0,0 +1,20 @@
+<script>
+import { store } from '../store';
+export default {
+   name: 'foundBanner',
+
+   data() {
+      return {
+         store,
+      }
+   }
+}
+</script>
+<template>
+   <div class="found_banner d-flex align-items-center w-100 bg-dark text-light p-3">
+      <h6>Found {{ store.cards.length }} cards</h6>
+   </div>
+</template>
+
+
+<style lang="scss" scoped></style>
