17aa2ba7a2244700db9b241961bcca5ef3a7622b
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-17 17:47:31 +0200

    create select item component

---
+++ src/components/selectItem.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+   name: 'selecItem',
+
+}
+</script>
+<template>
+   <div class="container px-2 py-4">
+      <select class="form-select" aria-label="default">
+         <option selected>Filter</option>
+         <option value="1">Alien</option>
+         <option value="2">???</option>
+         <option value="3">???</option>
+      </select>
+   </div>
+</template>
+
+
+<style lang="scss" scoped></style>
